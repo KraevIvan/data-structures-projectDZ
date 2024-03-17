fc6fe8ae2cceecb59b8ebc0947a14a33eb84f61d
KraevIvan <KraevIvanA@yandex.ru>
2024-03-17 22:42:50 +0300

    add tests for methods to_list and get_data_by_id

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -11,6 +11,12 @@ ll.insert_at_end({'id': 2})
 ll.insert_at_end({'id': 3})
 ll.insert_beginning({'id': 0})
 
+ll2 = LinkedList()
+ll2.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+ll2.insert_at_end({'id': 2, 'username': 'mik.roz'})
+ll2.insert_at_end({'id': 3, 'username': 'mosh_s'})
+ll2.insert_beginning({'id': 0, 'username': 'serebro'})
+
 
 class TestNodeAndLinkedList(unittest.TestCase):
 
@@ -20,7 +26,15 @@ class TestNodeAndLinkedList(unittest.TestCase):
         self.assertEqual(node_for_test2.next_node, node_for_test1)
 
     def test_Linked_list(self):
-        assert str(ll) == "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None"
+        self.assertEqual(str(ll), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
+
+    def test_to_list(self):
+        lst = ll.to_list()
+        self.assertEqual(str(lst), "[{'id': 0}, {'id': 1}, {'id': 2}, {'id': 3}]")
+
+    def test_get_data_by_id(self):
+        user_data = ll2.get_data_by_id(3)
+        self.assertEqual(user_data, {'id': 3, 'username': 'mosh_s'})
 
 
 if __name__ == '__main__':
